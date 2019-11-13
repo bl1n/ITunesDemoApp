6b81b49428ae342cb115d28135684f424f28caa6
sukha <sukhanov87@gmail.com>
2019-11-13 12:55:00 +0300

    repository

---
+++ app/src/main/java/team/lf/itunesdemoapp/repository/ITunesRepository.kt
@@ -0,0 +1,28 @@
+package team.lf.itunesdemoapp.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import team.lf.itunesdemoapp.database.ITunesDemoDatabase
+import team.lf.itunesdemoapp.database.asDomainModel
+import team.lf.itunesdemoapp.domain.DomainModel
+import team.lf.itunesdemoapp.network.ITunesApi
+import team.lf.itunesdemoapp.network.NetworkContainer
+import team.lf.itunesdemoapp.network.asDBModel
+
+class ITunesRepository(private val database: ITunesDemoDatabase) {
+
+    val results: LiveData<List<DomainModel>>  =
+        Transformations.map(database.iTunesDemoDao.getSearchResults()){
+        it.asDomainModel()
+    }
+
+    suspend fun refreshSearchList(term: String){
+        withContext(Dispatchers.IO){
+            val searchList: NetworkContainer = ITunesApi.retrofitService.getCollectionsByColeectionName(term).await()
+            database.iTunesDemoDao.insertAll(searchList.asDBModel())
+        }
+    }
+}
