9e765805103c4b9f12b2d80e188ad6da1db56d5e
sukha <sukhanov87@gmail.com>
2019-11-13 12:31:29 +0300

    API service

---
+++ app/src/main/java/team/lf/itunesdemoapp/network/ITunesApiService.kt
@@ -0,0 +1,47 @@
+package team.lf.itunesdemoapp.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://itunes.apple.com/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface ITunesApiService {
+    @GET("search")
+    fun getCollectionsByColeectionName(
+        @Query("term") term: String,
+        @Query("entity") entity: String = "album",
+        @Query("attribute") attribute: String = "albumTerm"
+    ) : Deferred<NetworkContainer>
+}
+
+object ITunesApi{
+    val retrofitService: ITunesApiService by lazy { retrofit.create(ITunesApiService::class.java) }
+}
+
+
+//    https://itunes.apple.com/lookup?entity=album&id=214403406
+//    https://itunes.apple.com/lookup?entity=song&id=1459409129
+//    https://itunes.apple.com/search?media=album&entity=album&term=Between
+//    https://itunes.apple.com/lookup?entity=song&id=214403406
+
+// https://itunes.apple.com/search?term=between&entity=album - ищет по term альбомы,
+// у которых либо автор, либо название cодержит term
+
+//https://itunes.apple.com/search?term=between&entity=album&attribute=albumTerm ищет по term альбомы,
+//// у которых  либо название cодержит term
