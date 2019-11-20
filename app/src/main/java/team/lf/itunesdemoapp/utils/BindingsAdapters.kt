1825465b2698b31bd143090260673e89b7b3ad1a
sukha <sukhanov87@gmail.com>
2019-11-20 10:15:23 +0300

    li_track

--- app/src/main/java/team/lf/itunesdemoapp/utils/BindingsAdapters.kt
+++ app/src/main/java/team/lf/itunesdemoapp/utils/BindingsAdapters.kt
@@ -8,9 +8,21 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import team.lf.itunesdemoapp.R
 
+@BindingAdapter("HQImageCover")
+fun bindHQImageCover(imgView: ImageView, imgUrl: String?) {
+    val image = imgUrl?.replace("100x100bb", "313x0w") // to get a good image resource
+    image?.let {
+        val imgUri = image.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                    .error(R.drawable.ic_broken_image))
+            .into(imgView)
+    }
+}
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
-//    val image = imgUrl?.replace("100x100bb", "313x0w") // to get a good image resource
     imgUrl?.let {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
@@ -21,4 +33,5 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             .into(imgView)
     }
 }
+
 
